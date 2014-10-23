ba049254c59b874873aa76968bfec1ccae88ad60
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-10-23 15:24:46 -0300

    directivas

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 (function() {
-	var app = angular.module("hello", []);
+	var app = angular.module("app", []);
 	app.controller('helloController', function() {
 		this.helloWorld = "Hello World!!";
 	});
+	app.controller('directives', function() {
+		this.client = {name: "marcelo", can:true};
+		this.customer = {name: "aida", can:false};
+	})
 })();
