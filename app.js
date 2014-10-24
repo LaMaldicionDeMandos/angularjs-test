8b6ae4fc2cf22beba00a4f6faa39dda64dff2bfc
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-10-24 10:49:19 -0300

    agrego un filtro para formatear

--- app.js
+++ app.js
@@ -4,6 +4,6 @@
 		this.helloWorld = "Hello World!!";
 	});
 	app.controller('directives', function() {
-		this.clients = [{name: "marcelo", can:true}, {name: "aida", can:false}];
+		this.clients = [{name: "marcelo", price: 2,can:true}, {name: "aida", price:2.5, can:false}];
 	})
 })();
